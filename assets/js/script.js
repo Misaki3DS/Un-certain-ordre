23c57c879799a41778c7da631286059006acc3a1
Misaki3DS <36511391+Misaki3DS@users.noreply.github.com>
2021-10-28 22:26:23 +0200

    Ajout des fichiers

---
+++ assets/js/script.js
@@ -0,0 +1,118 @@
+jQuery(function ($) {
+    document.getElementById("toolStart_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./assets/html/p1.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolAnime_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-1.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolIndex_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-1-1.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolLegal1_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-1-1-1.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolIllegal1_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-1-1-2.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolRailgun_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-1-2.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolIllLegal_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-1-2-1.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolIllLegal2_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-1-2-1.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolAccel_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-1-3.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolChrono_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-1-4.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolLivre_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-2.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolManga_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-2-1.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolMangaL_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-2-1-1.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolMangaIll_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-2-1-2.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolLN_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-2-2.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolLegal2_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-2-2-1.html";
+    }
+});
+
+jQuery(function ($) {
+    document.getElementById("toolIllegal2_bt").addEventListener("click", myfunction);
+    function myfunction() {
+        window.location.href="./p1-2-2-2.html";
+    }
+});
